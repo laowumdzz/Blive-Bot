fcb9428497375f907c4359b2b9847be8939f844e
laowumdzz <menboid@qq.com>
2026-02-06 21:09:08 +0800

    refactor:使用ruff格式化代码,更改为获取到用户名时的错误处理逻辑.fix:更人性化的错误提示

--- tests/live_rooms.py
+++ tests/live_rooms.py
@@ -1,108 +1,164 @@
 import ast
 import asyncio
+from email.message import EmailMessage
+from itertools import zip_longest
 import os
+from pathlib import Path
 import smtplib
 import ssl
-from email.message import EmailMessage
-from pathlib import Path
 from typing import Any
 
 import aiohttp
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from dotenv import load_dotenv
+from loguru import logger
 
-load_dotenv()
+load_dotenv(verbose=True)
 scheduler = AsyncIOScheduler()
 
 
-def convert_str_to_list(str_list) -> list[int] | None:
+def convert_str_to_list_int(v) -> list[int] | None:
     try:
-        result = ast.literal_eval(str_list)
-        if isinstance(result, list):
+        result = ast.literal_eval(v)
+        if isinstance(result, list) and all(isinstance(i, int) for i in result):
             return result
-        return None
-    except (ValueError, SyntaxError, TypeError):
-        return None
+    except (ValueError, SyntaxError):
+        logger.error(
+            "LIVE_ROOM_MID must be a valid list of integers, e.g., '[1, 2, 3]'")
+        raise
+    return None
 
 
-temp_path_str = os.getenv("TEMP_PATH", None)
-TEMP_PATH = Path(temp_path_str) if temp_path_str else Path.cwd()
-
-status_url = 'https://api.live.bilibili.com/room/v1/Room/getRoomInfoOld?mid={}'
-user_card = 'https://api.bilibili.com/x/web-interface/card?mid={}'
-uid = convert_str_to_list(os.getenv('LIVE_ROOM_MID'))
-if not uid:
-    raise RuntimeError('没有指定UID')
-status_urls = {status_url.format(x) for x in uid}
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0',
-    'Referer': 'https://www.bilibili.com/'
+ENV_TEMP_DIR = os.getenv("TEMP_PATH")
+TEMP_PATH = Path(ENV_TEMP_DIR) if ENV_TEMP_DIR else Path.cwd() / "temp"
+LIVE_STATUS_API = "https://api.live.bilibili.com/room/v1/Room/getRoomInfoOld?mid={}"
+USER_CARD_API = "https://api.live.bilibili.com/live_user/v1/Master/info?uid={}"
+GLOBAL_SESSION: aiohttp.ClientSession | None = None
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 "
+    "Safari/537.36 Edg/145.0.0.0",
+    "Referer": "https://www.bilibili.com/",
 }
+CodeError = Exception
+
+
+UIDS = convert_str_to_list_int(os.getenv("LIVE_ROOM_MID"))
+if not UIDS:
+    raise ValueError("没有指定UID, 请在.env文件中指定LIVE_ROOM_MID,示例:[uid1, uid2]")
+live_status_urls = [LIVE_STATUS_API.format(uid) for uid in UIDS]
+user_care_urls = [USER_CARD_API.format(uid) for uid in UIDS]
 
 
 def qqyx(subject: str, message: str) -> None:
-    email_addr = 'menboid@qq.com'
-    email_pwd = 'zxkcqcnuanrachdd'
+    email_addr = "menboid@qq.com"
+    email_pwd = "zxkcqcnuanrachdd"
 
     if not email_addr or not email_pwd:
         raise ValueError("邮箱地址和密码不能为空")
 
     msg = EmailMessage()
-    msg['subject'] = subject
-    msg['From'] = email_addr
-    msg['To'] = email_addr
+    msg["subject"] = subject
+    msg["From"] = email_addr
+    msg["To"] = email_addr
     msg.set_content(message)
 
     try:
         with smtplib.SMTP_SSL("smtp.qq.com", 465, context=ssl.create_default_context()) as smtp:
             smtp.login(email_addr, email_pwd)
             smtp.send_message(msg)
-        print("邮件发送成功")
+        logger.success("成功发送邮件")
     except smtplib.SMTPResponseException as e:
-        print(f"SMTP错误代码：{e.smtp_code}, 错误信息：{e.smtp_error}")
+        logger.warning(f"SMTP错误代码：{e.smtp_code}, 错误信息：{e.smtp_error}")
     except smtplib.SMTPException as e:
-        print(f"SMTP异常：{e}")
+        logger.opt(exception=e).error("SMTP异常")
     except Exception as e:
-        print(f"发送邮件时发生未知错误：{e}")
+        logger.opt(exception=e).error("未知错误")
 
 
 async def fetch(session, url) -> dict[str, Any]:
     async with session.get(url) as response:
-        return await response.json()
+        response.raise_for_status()
+        json_data = await response.json()
+        if not json_data.get("code"):
+            return json_data
+        raise CodeError("风控校验失败")
 
 
-@scheduler.scheduled_job('interval', seconds=6, misfire_grace_time=10, max_instances=2)
+async def fetch_all_data(session):
+    """并发获取所有直播间和用户卡片数据"""
+    status_tasks = [fetch(session, url) for url in live_status_urls]
+    user_card_tasks = [fetch(session, url) for url in user_care_urls]
+    status_results = await asyncio.gather(*status_tasks, return_exceptions=True)
+    user_card_results = await asyncio.gather(*user_card_tasks, return_exceptions=True)
+    user_card_results = [CodeError("1"), CodeError("2")]
+    return status_results, user_card_results
+
+
+@scheduler.scheduled_job("interval", seconds=5, max_instances=2)
 async def get_live_status():
-    print("-" * 20)
+    if GLOBAL_SESSION is None:
+        logger.error("全局 ClientSession 尚未初始化！")
+        return
     try:
-        async with aiohttp.ClientSession(headers=headers) as session:
-            results = await asyncio.gather(*(fetch(session, url) for url in status_urls), return_exceptions=True)
-            for mid, info in zip(uid, results):
-                if isinstance(info, dict):
-                    path = TEMP_PATH / str(mid)
-                    user_name = (await fetch(session, user_card.format(mid)))["data"]["card"]["name"]
-                    print(f"[{user_name}]{'已开播' if path.exists() else '未开播'}")
-                    if info['data']['liveStatus'] and not path.exists():
-                        qqyx("开播提醒", f"[{user_name}]已开播")
-                        path.touch()
-                    if not info['data']['liveStatus'] and path.exists():
-                        qqyx('下播提醒', f'[{user_name}]已下播')
-                        path.unlink(missing_ok=True)
-                else:
-                    print(f"[{mid}]获取状态出错 e:{info}")
+        status_results, user_card_results = await fetch_all_data(GLOBAL_SESSION)
+
+        processed_status_results = []
+        for i, result in enumerate(status_results):
+            if isinstance(result, Exception):
+                logger.opt(exception=result).error(
+                    f"获取状态URL失败. UID: [{UIDS[i]}] | URL: {live_status_urls[i]}")
+                processed_status_results.append(None)
+            else:
+                processed_status_results.append(result)
+
+        processed_user_card_results = []
+        for i, result in enumerate(user_card_results):
+            if isinstance(result, Exception):
+                logger.warning(f"获取用户名称失败,回退到使用UID显示. URL: {user_care_urls[i]} 错误原因: {type(result).__name__}: {result}")
+                processed_user_card_results.append(None)
+            else:
+                processed_user_card_results.append(result)
+
+        for uid, status_data, name_data in zip_longest(UIDS, processed_status_results, processed_user_card_results):
+            if status_data is None:
+                logger.warning(f"用户 {uid} 的数据不完整，已跳过处理。")
+                continue
+            path = TEMP_PATH / str(uid)
+            user_name = name_data["data"]["info"]["uname"] if name_data else uid
+            logger.info(f"{[user_name]}{'已开播' if path.exists() else '未开播'}")
+            if status_data["data"]["liveStatus"] and not path.exists():
+                qqyx("开播提醒", f"[{user_name}]已开播")
+                path.touch()
+            if not status_data["data"]["liveStatus"] and path.exists():
+                qqyx("下播提醒", f"[{user_name}]已下播")
+                path.unlink(missing_ok=True)
+        logger.info("所有直播间检测完毕")
     except asyncio.CancelledError:
         pass
+    except Exception as e:
+        logger.opt(exception=e).error("未知错误")
 
 
-async def main():
+async def run():
+    global GLOBAL_SESSION
+    scheduler.start()
+    logger.success("调度器已启动, 按[Ctrl+C]退出")
+    if GLOBAL_SESSION is None:
+        GLOBAL_SESSION = aiohttp.ClientSession(headers=HEADERS)
     try:
-        scheduler.start()
         await asyncio.Event().wait()
-    except asyncio.CancelledError:
-        pass
+    except (asyncio.CancelledError, KeyboardInterrupt):
+        logger.info("正在关闭调度器和会话...")
+        scheduler.shutdown()
+        if GLOBAL_SESSION is not None:
+            await GLOBAL_SESSION.close()
+            GLOBAL_SESSION = None
+        logger.success("调度器和会话已关闭")
 
 
-if __name__ == '__main__':
-    asyncio.run(main())
+if __name__ == "__main__":
+    try:
+        import uvloop
+        uvloop.run(run())
+    except ModuleNotFoundError:
+        asyncio.run(run())
